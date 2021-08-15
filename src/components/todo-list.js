3c06ae1705ff4b60fef95781d7fd9e69f5d6e3ea
Marina Grishina <52827985+AniramOcean@users.noreply.github.com>
2021-08-15 22:12:53 +0300

    feat: add props for todoList and todoListItem with rest operator

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import TodoListItem from './todo-list-item';
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
+    const items = todos.map((item) => {
+        const { id, ...itemProps } = item;
+
+        return (
+            <li key={ id }>
+                <TodoListItem { ...itemProps } />
+            </li>
+        )
+    });
+
     return (
         <ul>
-            <li><TodoListItem /></li>
-            <li><TodoListItem /></li>
+            { items }
         </ul>
     )
 }
